2bb91281f3f0965c05abf99c0ea1af069c82527c
Iann-rst <iannrst11@gmail.com>
2022-05-17 20:06:19 -0300

    App: carregando fontes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import * as SplashScreen from 'expo-splash-screen';
 import { ThemeProvider } from 'styled-components';
 
 import { Dashboard } from './src/screens/Dashboard';
 import theme from './src/global/styles/theme';
 
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
