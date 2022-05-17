22940303f0208249c509d3ba453bb66f33f2f657
Iann-rst <iannrst11@gmail.com>
2022-05-17 18:57:53 -0300

    Dashboard

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,10 +4,12 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+
+  background-color: ${({ theme }) => theme.colors.background}
 `;
 
 export const Title = styled.Text`
   font-size: 24px;
   font-weight: bold;
-  color: purple;
+  color: ${({ theme }) => theme.colors.title};
 `;
