85ec4894a10cddb644eb821b81a8bf74b56ce90e
Iann-rst <iannrst11@gmail.com>
2022-05-19 19:06:13 -0300

    style: adicionando tipagem e propriedades

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,9 +1,15 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { RFValue } from 'react-native-responsive-fontsize';
 
-export const Container = styled.View`
-  background-color: ${({ theme }) => theme.colors.shape};
+interface TypeProps {
+  type: 'up' | 'down' | 'total';
+}
+
+export const Container = styled.View<TypeProps>`
+  background-color: ${({ theme, type }) =>
+    type === 'total' ? theme.colors.secondary : theme.colors.shape};
+
   width: ${RFValue(300)}px;
   border-radius: 5px;
 
@@ -17,30 +23,48 @@ export const Header = styled.View`
   justify-content: space-between;
 `;
 
-export const Title = styled.Text`
+export const Title = styled.Text<TypeProps>`
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(14)}px;
-  color: ${({ theme }) => theme.colors.text_dark}
+
+  color: ${({ theme, type }) =>
+    type === 'total' ? theme.colors.shape : theme.colors.text_dark};
 `;
 
-export const Icon = styled(FontAwesome5)`
+export const Icon = styled(FontAwesome5) <TypeProps>`
   font-size: ${RFValue(40)}px;
+
+  ${(props) => props.type === 'up' && css`
+    color: ${({ theme }) => theme.colors.success}
+  ` };
+  
+  ${(props) => props.type === 'down' && css`
+  color: ${({ theme }) => theme.colors.attention}
+  ` };
+
+  ${(props) => props.type === 'total' && css`
+  color: ${({ theme }) => theme.colors.shape}
+  ` };
   
 `;
 
 export const Footer = styled.View`
 `;
 
-export const Amount = styled.Text`
+export const Amount = styled.Text<TypeProps>`
   font-family: ${({ theme }) => theme.fonts.medium};
   font-size:${RFValue(32)}px;
-  color: ${({ theme }) => theme.colors.text_dark};
+
+  color: ${({ theme, type }) =>
+    type === 'total' ? theme.colors.shape : theme.colors.text_dark};
 
   margin-top: 38px;
 `;
 
-export const LastTransaction = styled.Text`
+export const LastTransaction = styled.Text<TypeProps>`
   font-family: ${({ theme }) => theme.fonts.regular};
   font-size:${RFValue(12)}px;
-  color: ${({ theme }) => theme.colors.text};
+
+  color: ${({ theme, type }) =>
+    type === 'total' ? theme.colors.shape : theme.colors.text};
 `;
