ccd53e552dbf946ee8b7b146db3ab58a34ce4ed2
Iann-rst <iannrst11@gmail.com>
2022-05-19 19:07:21 -0300

    Feat:  adicionando tipagem no componente Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -10,17 +10,30 @@ import {
   LastTransaction
 } from './styles';
 
-export function Card() {
+interface ICard {
+  title: string;
+  amount: string;
+  lastTransaction: string;
+  type: 'up' | 'down' | 'total';
+}
+
+const icon = {
+  up: 'arrow-alt-circle-up',
+  down: 'arrow-alt-circle-down',
+  total: 'dollar-sign'
+}
+
+export function Card({ type, title, amount, lastTransaction }: ICard) {
   return (
-    <Container>
+    <Container type={type}>
       <Header>
-        <Title>Entradas</Title>
-        <Icon name="arrow-alt-circle-up" />
+        <Title type={type}>{title}</Title>
+        <Icon name={icon[type]} type={type} />
       </Header>
 
       <Footer>
-        <Amount>R$ 17.400,00</Amount>
-        <LastTransaction>Última entrada dia 13 de abril </LastTransaction>
+        <Amount type={type}>{amount}</Amount>
+        <LastTransaction type={type}>{lastTransaction} </LastTransaction>
       </Footer>
     </Container>
   )
