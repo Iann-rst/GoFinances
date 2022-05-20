ec0ae760d4b6d0522f26cdc9b7cc75b8389b2e71
Iann-rst <iannrst11@gmail.com>
2022-05-20 20:03:56 -0300

    Screen Register

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { ThemeProvider } from 'styled-components';
 
 import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
 import theme from './src/global/styles/theme';
 
 import {
@@ -28,7 +29,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   )
 }
