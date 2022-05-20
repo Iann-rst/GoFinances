ae24fbc51c759c6957cf51fd3ca0bfe81f8a5888
Iann-rst <iannrst11@gmail.com>
2022-05-19 21:14:01 -0300

    Componente TransactionCard

---
+++ src/components/TransactionCard/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { Container, Title, Amount, Footer, Category, Icon, CategoryName, Date } from './styles';
+
+interface Category {
+  name: string;
+  icon: string;
+}
+
+interface Data {
+  title: string;
+  amount: string;
+  category: Category;
+  date: string;
+}
+
+interface Props {
+  data: Data;
+}
+
+export function TransactionCard({ data }: Props) {
+  return (
+    <Container>
+      <Title>{data.title}</Title>
+      <Amount>{data.amount}</Amount>
+
+      <Footer>
+        <Category>
+          <Icon name={data.category.icon} />
+          <CategoryName>{data.category.name}</CategoryName>
+        </Category>
+        <Date>{data.date}</Date>
+      </Footer>
+    </Container>
+  )
+}
---
+++ src/components/TransactionCard/styles.ts
@@ -0,0 +1,56 @@
+import { RFValue, RFPercentage } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
+import { FontAwesome5 } from '@expo/vector-icons'
+
+export const Container = styled.View`
+  border-radius: 5px;
+  background-color: ${({ theme }) => theme.colors.shape};
+
+  padding: 17px 24px;
+  margin-bottom: 16px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(14)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.text_dark}
+`;
+
+export const Amount = styled.Text`
+  font-size: ${RFValue(20)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.success};
+  margin-top: 2px;
+`;
+
+export const Footer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-top: 19px;
+`;
+
+export const Category = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Icon = styled(FontAwesome5)`
+  font-size: ${RFValue(20)}px;
+  color: ${({ theme }) => theme.colors.text}
+`;
+
+export const CategoryName = styled.Text`
+  font-size: ${RFValue(14)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.text};
+
+  margin-left: 17px;
+`;
+
+export const Date = styled.Text`
+  font-size: ${RFValue(14)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.text}
+`;
