c83ccfa49c120ee5a606bfbc8f04ed21096ec0bf
Iann-rst <iannrst11@gmail.com>
2022-05-23 18:22:54 -0300

    Screen Register: add Modal

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,18 +1,42 @@
 import React, { useState } from 'react';
+import { Modal } from 'react-native';
 
 import { Button } from '../../components/Forms/Button';
-import { CategorySelect } from '../../components/Forms/CategorySelect';
+import { CategorySelectButton } from '../../components/Forms/CategorySelectButton';
 import { Input } from '../../components/Forms/Input';
 import { TransactionTypeButton } from '../../components/Forms/TransactionTypeButton';
 
-import { Container, Header, Title, Form, Fields, TransactionType } from './styles';
+import { CategorySelect } from '../CategorySelect';
+
+
+import {
+  Container,
+  Header,
+  Title,
+  Form,
+  Fields,
+  TransactionType
+} from './styles';
 
 export function Register() {
-
   const [transactionType, setTransactionType] = useState('');
+  const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+
+  const [category, setCategory] = useState({
+    key: 'category',
+    name: 'Categoria'
+  });
 
   function handleTransactionTypeSelect(type: 'up' | 'down') {
     setTransactionType(type);
+  }
+
+  function handleCloseSelectCategoryModal() {
+    setCategoryModalOpen(false);
+  }
+
+  function handleOpenSelectCategoryModal() {
+    setCategoryModalOpen(true);
   }
 
   return (
@@ -45,12 +69,23 @@ export function Register() {
             />
           </TransactionType>
 
-          <CategorySelect title="Categoria" />
+          <CategorySelectButton
+            title={category.name}
+            onPress={handleOpenSelectCategoryModal}
+          />
 
         </Fields>
 
         <Button title="Enviar" />
       </Form >
+
+      <Modal visible={categoryModalOpen}>
+        <CategorySelect
+          category={category}
+          setCategory={setCategory}
+          closeSelectCategory={handleCloseSelectCategoryModal}
+        />
+      </Modal>
     </Container>
 
   );
