d2f0debc760b82d5a079bbf75a5161c0c7992601
Iann-rst <iannrst11@gmail.com>
2022-05-23 15:20:51 -0300

    Screen Register: Add Button (formulário)

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Button } from '../../components/Forms/Button';
 import { Input } from '../../components/Forms/Input';
 
-import { Container, Header, Title, Form } from './styles';
+import { Container, Header, Title, Form, Fields } from './styles';
 
 export function Register() {
   return (
@@ -11,13 +12,16 @@ export function Register() {
       </Header>
 
       <Form>
-        <Input
-          placeholder="Nome"
-        />
+        <Fields>
+          <Input
+            placeholder="Nome"
+          />
+          <Input
+            placeholder="Preço"
+          />
+        </Fields>
 
-        <Input
-          placeholder="Preço"
-        />
+        <Button title="Enviar" />
       </Form >
     </Container>
 
