2302c2595e2048e8ed37d130b284a92abc29a81b
Iann-rst <iannrst11@gmail.com>
2022-05-25 19:13:47 -0300

    Componente InputForm (input controlado)

---
+++ src/components/Forms/InputForm/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { TextInputProps } from 'react-native';
+import { Control, Controller } from 'react-hook-form';
+
+import { Input } from '../Input';
+
+interface Props extends TextInputProps {
+  control: Control;
+  name: string;
+}
+
+import { Container } from './styles';
+
+//Input controlado, usando react hook form
+export function InputForm({
+  control,
+  name,
+  ...rest
+}: Props) {
+  return (
+    <Container>
+      <Controller
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <Input
+            onChangeText={onChange}
+            value={value}
+            {...rest}
+          />
+        )}
+        name={name}
+      />
+    </Container>
+  )
+}
