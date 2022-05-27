fc761e5d7deed8fb33e5e67ec46ea382d58a7bf3
Iann-rst <iannrst11@gmail.com>
2022-05-27 20:39:59 -0300

    Criando rotas para navegação em tabs

--- App.tsx
+++ App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 import { ThemeProvider } from 'styled-components';
 
-import { Dashboard } from './src/screens/Dashboard';
-import { Register } from './src/screens/Register';
+
 import theme from './src/global/styles/theme';
+import { AppRoutes } from './src/routes/app.routes';
+
+import { NavigationContainer } from '@react-navigation/native';
 
 import {
   useFonts,
@@ -29,7 +31,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
