90ca31da5fa04050ab1f2a4512767d5ceff88764
Iann-rst <iannrst11@gmail.com>
2022-05-30 19:34:33 -0300

    fix interface TransactionCardProps

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
+import { categories } from '../../utils/categories';
 
 import { Container, Title, Amount, Footer, Category, Icon, CategoryName, Date } from './styles';
 
-interface Category {
-  name: string;
-  icon: string;
-}
 
 export interface TransactionCardProps {
   type: 'positive' | 'negative';
-  title: string;
+  name: string;
   amount: string;
-  category: Category;
+  category: string;
   date: string;
 }
 
@@ -20,9 +17,13 @@ interface Props {
 }
 
 export function TransactionCard({ data }: Props) {
+  const category = categories.filter(
+    item => item.key === data.category
+  )[0];
+
   return (
     <Container>
-      <Title>{data.title}</Title>
+      <Title>{data.name}</Title>
       <Amount type={data.type}>
         {
           data.type === 'negative' && '- '}
@@ -31,8 +32,8 @@ export function TransactionCard({ data }: Props) {
 
       <Footer>
         <Category>
-          <Icon name={data.category.icon} />
-          <CategoryName>{data.category.name}</CategoryName>
+          <Icon name={category.icon} />
+          <CategoryName>{category.name}</CategoryName>
         </Category>
         <Date>{data.date}</Date>
       </Footer>
