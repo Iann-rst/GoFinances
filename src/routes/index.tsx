11439c926fe3be0c76f296e2f3e5381c9e3c077f
Iann-rst <iannrst11@gmail.com>
2022-06-01 20:46:34 -0300

    Rotas privadas e publicas (fix)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,6 @@ import { useAuth } from '../hooks/auth';
 
 export function Routes() {
   const { user } = useAuth();
-  console.log(user);
   return (
     <NavigationContainer>
       {user.id ? <AppRoutes /> : <AuthRoutes />}
