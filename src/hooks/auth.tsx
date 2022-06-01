35ff4c64e5f5c650d6d42fbd3e9e1163e24e1311
Iann-rst <iannrst11@gmail.com>
2022-06-01 19:23:53 -0300

    Autenticação e contexto

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,4 +1,7 @@
-import React, { createContext, ReactNode, useContext } from 'react';
+import React, { createContext, ReactNode, useContext, useState } from 'react';
+import * as AppleAuthentication from 'expo-apple-authentication';
+
+import * as AuthSession from 'expo-auth-session';
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -13,20 +16,81 @@ interface User {
 
 interface IAuthContextData {
   user: User;
+  signInWithGoogle(): Promise<void>;
+  signInWithApple(): Promise<void>;
 }
+
+interface AuthorizationResponse {
+  params: {
+    access_token: string;
+  }
+  type: string;
+}
+
+//Variáveis de ambiente
+const { CLIENT_ID } = process.env;
+const { REDIRECT_URI } = process.env;
 
 const AuthContext = createContext({} as IAuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
-  const user = {
-    id: '1212',
-    name: 'Iann Rodrigues',
-    email: 'iann@email.com',
+  const [user, setUser] = useState<User>({} as User);
+
+  async function signInWithGoogle() {
+    try {
+      const RESPONSE_TYPE = 'token';
+      const SCOPE = encodeURI('profile email');
+
+      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+      const { type, params } = await AuthSession.startAsync({ authUrl }) as AuthorizationResponse;
+
+      if (type === 'success') {
+        const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`);
+        const userInfo = await response.json();
+
+        setUser({
+          id: userInfo.id,
+          email: userInfo.email,
+          name: userInfo.given_name,
+          photo: userInfo.picture
+        });
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async function signInWithApple() {
+    console.log("SignInWithApple");
+    // try {
+    //   const credential = await AppleAuthentication.signInAsync({
+    //     requestedScopes: [
+    //       AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+    //       AppleAuthentication.AppleAuthenticationScope.EMAIL,
+    //     ]
+    //   });
+    //   console.log(credential);
+    //   if (credential) {
+    //     const userLogged = {
+    //       id: String(credential.user),
+    //       email: credential.email,
+    //       name: credential.fullName!.givenName!,
+    //       photo: undefined
+    //     };
+    //     setUser(userLogged);
+    //   }
+    // } catch (error) {
+    //   throw new Error(error);
+    // }
   }
 
   return (
-    <AuthContext.Provider
-      value={{ user }}
+    <AuthContext.Provider value={{
+      user,
+      signInWithGoogle,
+      signInWithApple
+    }}
     >
       {children}
     </AuthContext.Provider>)
