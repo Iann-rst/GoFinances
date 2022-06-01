a5d4972b86345825e60b16863aa94fd6a13efedd
Iann-rst <iannrst11@gmail.com>
2022-06-01 15:24:15 -0300

    Screen SignIn

---
+++ src/screens/SignIn/styles.ts
@@ -0,0 +1,52 @@
+import { RFValue, RFPercentage } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 70%;
+
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  justify-content: flex-end;
+  align-items:center;
+`;
+
+export const TitleWrapper = styled.View`
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.medium};
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(30)}px;
+
+  text-align: center;
+  margin-top: ${RFValue(40)}px;
+
+`;
+
+export const SignInTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(16)}px;
+
+  text-align: center;
+  margin-top: ${RFValue(80)}px;
+  margin-bottom: ${RFValue(67)}px;
+`;
+
+export const Footer = styled.View`
+  width: 100%;
+  height: 30%;
+  background-color: ${({ theme }) => theme.colors.secondary};
+`;
+
+export const FooterWrapper = styled.View`
+  margin-top: ${RFPercentage(-4)}px;
+  padding: 0 32px;
+  justify-content: space-between;
+`;
