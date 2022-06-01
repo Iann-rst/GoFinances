69c76921c4e34a6b210eaedde15d2ba5cfae442a
Iann-rst <iannrst11@gmail.com>
2022-06-01 16:10:27 -0300

    Criando um contexto

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,42 @@
+import React, { createContext, ReactNode, useContext } from 'react';
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  photo?: string;
+}
+
+interface IAuthContextData {
+  user: User;
+}
+
+const AuthContext = createContext({} as IAuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const user = {
+    id: '1212',
+    name: 'Iann Rodrigues',
+    email: 'iann@email.com',
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ user }}
+    >
+      {children}
+    </AuthContext.Provider>)
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+export { AuthProvider, useAuth }
+
