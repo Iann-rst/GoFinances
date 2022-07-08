12cac8a7d3e6572fe66fe3ebc906735673d2d607
Iann-rst <iannrst11@gmail.com>
2022-07-08 12:56:00 -0300

    Abstraindo Configurações

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/**Arquivo de configuração do jest */
+module.exports = {
+  testPathIgnorePatterns: [
+    "/node_modules",
+    "/android",
+    "/ios"
+  ],
+  preset: "jest-expo",
+  setupFilesAfterEnv: [
+    "@testing-library/jest-native/extend-expect"
+  ]
+}
